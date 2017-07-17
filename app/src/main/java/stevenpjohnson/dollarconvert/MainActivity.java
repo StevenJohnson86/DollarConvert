118cf3313cdba989994a90a011083a00c3036a06
Steven Johnson <stevenjohnson86@gmail.com>
2017-07-17 15:14:24 -0700

    add EUR and GBP to app. Add Spinner for user to pick option.

--- app/src/main/java/stevenpjohnson/dollarconvert/MainActivity.java
+++ app/src/main/java/stevenpjohnson/dollarconvert/MainActivity.java
@@ -5,33 +5,44 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
-    public void cnvToJPY(View view){
 
-        Log.i("Info", "*****inside cnvToJPY start");
 
-        //get currInputView val
+    public void cnvCurr(View view){
+
+        HashMap<String, Double> currRates = new HashMap<String, Double>();
+        //in future TODO: populate rates from api call
+        currRates.put("JPY", 112.66);
+        currRates.put("EUR", 0.87);
+        currRates.put("GBP", 0.77);
+
+        //get currInputView
         EditText currInput = (EditText) findViewById(R.id.currInputView);
 
         if(currInput.getText().toString().length() == 0){
-            Log.i("Info", "*****inside currInput if");
+
             Toast.makeText(this, "You must enter a value", Toast.LENGTH_LONG).show();
-            return;
+
         } else {
-            Log.i("Info", "*****inside currInput else");
-            Log.i("Info", "***" + currInput.getText().toString() + "***");
+
             double currInputVal = Double.parseDouble(currInput.getText().toString());
 
+            //get target currency code from spinner
+            Spinner currCodes = (Spinner) findViewById(R.id.resCurr);
+
             //multiply by conversion rate
-            currInputVal = currInputVal * 112.66;
+            currInputVal = currInputVal * currRates.get(currCodes.getSelectedItem());
 
             //display returned val to newCurrView
             TextView currOutput = (TextView) findViewById(R.id.newCurrView);
-            currOutput.setText(String.valueOf(currInputVal));
+            currOutput.setText(String.valueOf(currInputVal) + " " + currCodes.getSelectedItem());
         }
     }
 
